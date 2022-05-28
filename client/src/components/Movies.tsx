9c18a0b2427271896ee4a0e91fa59feb0a62838d
emewjin <emewjin@gmail.com>
2022-05-28 19:09:28 +0900

    useApolloClient

--- client/src/components/Movies.tsx
+++ client/src/components/Movies.tsx
@@ -1,7 +1,47 @@
+import { gql, useApolloClient } from '@apollo/client';
+import { useEffect, useState } from 'react';
+
+type Movie = {
+  id: string;
+  title: string;
+};
+
 const Movies = () => {
+  const client = useApolloClient();
+
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    async function getMovies() {
+      const {
+        data: { allMovies },
+        loading,
+      } = await client.query({
+        query: gql`
+          {
+            allMovies {
+              title
+              id
+            }
+          }
+        `,
+      });
+      setMovies(allMovies);
+      setIsLoading(loading);
+    }
+    getMovies();
+  }, [client]);
+
   return (
     <div>
       <h1 className="text-2xl">Movies</h1>
+      <p>{isLoading ? 'isLoading...' : ''}</p>
+      <ul>
+        {movies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
     </div>
   );
 };
